0fd05ad042f59b6040af4b1d0d17c44204c2ea87
EhanSohn <ehanjsohn@gmail.com>
2023-03-28 19:26:10 +0900

    photo to cartoon by Kmeans

---
+++ photoCartoonKmeans.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+import numpy as np
+
+# photo to cartoon using Kmeans
+img = cv.imread('flowers.png')
+imgTrans = img.reshape((-1, 3)).astype(np.float32)
+criteria = (cv.TERM_CRITERIA_EPS|cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+compactness, label, center = cv.kmeans(imgTrans, 25, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+center = np.uint8(center)
+cartoonKmeans = center[label.flatten()]
+cartoonKmeans = cartoonKmeans.reshape((img.shape))
+
+cv.imshow("Cartoon", cartoonKmeans)
+cv.waitKey(0)
+cv.destroyAllWindows()
